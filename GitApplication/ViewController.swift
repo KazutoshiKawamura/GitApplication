e019ba70d1af3e6f373f3f250983e67585dd6cfc
KazutoshiKawamura <kztskawamu@gmail.com>
2015-11-14 12:11:12 +0900

    initialize commit

---
+++ GitApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitApplication
+//
+//  Created by kztskawamu on 2015/11/14.
+//  Copyright © 2015年 cazcawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
