4eae2e6cee4edeaadec3dfe313ca7709629bd6a8
KazutoshiKawamura <kztskawamu@gmail.com>
2015-11-14 12:34:06 +0900

    カウント機能追加

--- GitApplication/ViewController.swift
+++ GitApplication/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var n = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func p() {
+        n++
+        label.text = String(n)
+    }
 
 }
 
